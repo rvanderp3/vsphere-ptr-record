f5c651cebafce85f884263c104d35428fe754cdb
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-08-29 16:02:43 -0400

    configure hosts file in tmp

--- gen-hosts.sh
+++ gen-hosts.sh
@@ -4,7 +4,7 @@ SUBNET_END=254
 
 for SUBNET in $(seq $SUBNET_START $SUBNET_END); do
     for IP in $(seq 1 255); do
-        echo "192.168.${SUBNET}.${IP} ${IP}.${SUBNET}.168.192.in-addr.arpa" >> hosts
+        echo "192.168.${SUBNET}.${IP} ${IP}.${SUBNET}.168.192.in-addr.arpa" >> /tmp/hosts
     done
 done
 
@@ -13,7 +13,7 @@ if [ -n ${SUBNETS_JSON} ]; then
     if [ -f ${SUBNETS_JSON} ]; then
         IPADDRESSES=$(cat ${SUBNETS_JSON} | jq -r .[].ipAddresses | grep -oP '"\K[^"]+' | grep -v ',')
         for IPADDRESS in ${IPADDRESSES}; do
-            echo "$IPADDRESS $(echo $IPADDRESS | rev).in-addr.arpa" >> hosts
+            echo "$IPADDRESS $(echo $IPADDRESS | rev).in-addr.arpa" >> /tmp/hosts
         done
     fi
 fi
